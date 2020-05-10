926bf2cb0e2adfee0e180d72e67de938aabf3cd1
Roxana Scutaru <rm.scutaru@gmail.com>
2020-05-10 15:32:59 +0300

    Unchecking a checkbox works now.

--- src/redux/reducers/filter.js
+++ src/redux/reducers/filter.js
@@ -5,12 +5,17 @@ const initialState = {
 export function filtersReducer(state = initialState, action) {
     switch (action.type) {
         case 'SET_FILTER':
+            let modifiedFilters = state.filters.filter((f)=> {
+                return f.filterName!==action.payload.filterName
+            });
+            
+            if(modifiedFilters.length===state.filters.length){
+                modifiedFilters.push(action.payload);
+            }
+
             const newState={
                 ...state,
-                filters: [
-                    ...state.filters,
-                    action.payload
-                ]
+                filters: modifiedFilters
             }
             return newState;
         default:
